a2b8ce7dbc4293ccb3aae56d5bcd9b65b4311688
Cherry <pinkcherim@gmail.com>
2022-01-05 18:01:40 +0900

    Add files via upload

---
+++ Ch022-3 변수와 입력/practice.py
@@ -0,0 +1,8 @@
+# format() 함수의 다양한 형태
+data1 = "{}만원".format(5000)
+data2 = "{}M, {}M {}M 달리기".format(100, 400, 800)
+
+# 인덱스 사용
+data3 = "Programming Language {} {} {}".format('Python', 'Script', '3.10.1')
+print(data1)
+print(data2)
